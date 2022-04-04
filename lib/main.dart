04df43de4630e4e4bf3a84b7a57c8d7711dd2597
Luca Vinciguerra <lvinci@outlook.de>
2022-04-04 12:37:47 +0200

    implement localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gbuds_controller/theme/theme.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const GbudsControllerApp());
@@ -11,8 +12,13 @@ class GbudsControllerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: gbudsControllerThemeLight,
       darkTheme: gbudsControllerThemeDark,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
+      home: const Scaffold(),
     );
   }
 }
